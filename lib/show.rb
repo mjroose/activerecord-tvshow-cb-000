936ea1ae9faa52f331098265f8047ee440ba7b96
Michael Roose <mjroose@gmail.com>
2018-09-30 18:09:55 +0000

    automatically backed up by learn

--- lib/show.rb
+++ lib/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.order('rating DESC').limit(1).first
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.order('rating ASC').limit(1).first
+  end
 end
