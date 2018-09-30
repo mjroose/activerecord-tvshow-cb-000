3161d97bfc7fe59a2f392b4ce853b37249717b9b
Michael Roose <mjroose@gmail.com>
2018-09-30 18:08:01 +0000

    automatically backed up by learn

--- lib/show.rb
+++ lib/show.rb
@@ -1,11 +1,9 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    resp = self.order(:'rating DESC').limit(1)
-    binding.pry
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
-    show_id = self.highest_rating
-    self.find(show_id)
+    self.order('rating DESC').limit(1)
   end
 end
