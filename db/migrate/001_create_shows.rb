88848929b7fe00104f96dc102356614e9614c8e7
Michael Roose <mjroose@gmail.com>
2018-09-30 17:46:24 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,11 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change
-    def create_table do |t|
-
+    def create_table :shows do |t|
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
+      t.string :season
     end
   end
 end
